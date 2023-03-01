aa2226f84ccf35ba86128bc29f22c58fa35a5f0c
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-01 01:17:25 -0300

    refac: removida a responsabilidade do IMC da classe paciente

--- sem01/tarefa/classes/Paciente.php
+++ sem01/tarefa/classes/Paciente.php
@@ -3,35 +3,18 @@
     namespace classes;
 
     class Paciente extends Pessoa{
-        private $imc;
 
         public function __construct($nome, $idade, $altura, $peso)
         {
             parent::__construct($nome, $idade, $altura, $peso);
         }
 
-        public function calcImc(){
-            if(is_numeric($this->altura) && is_numeric($this->peso)){
-                $this->imc = $this->peso / pow($this->altura, 2);
-            }else{
-                echo "\nIMC $this->nome: Erro, informe peso e altura\n";
-            }
-        }
-
-        public function showImc(){
-            if(is_numeric($this->imc)){
-                echo "\nO IMC de $this->nome é: " . number_format($this->imc, 2) . "\n";
-            }
-        }
-
         public function setAltura(float $altura){
             $this->altura = $altura;
-            $this->calcImc();
         }
 
         public function setPeso(float $peso){
             $this->peso = $peso;
-            $this->calcImc();
         }
 
         public function __set($nome, $valor){
