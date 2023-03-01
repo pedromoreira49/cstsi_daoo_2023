cd5cdbf4986e442b66000e19a59f24965db74edb
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-01 01:18:40 -0300

    refac: removida funções de calcular Imc do index e adicionada na classe imc

--- sem01/tarefa/index.php
+++ sem01/tarefa/index.php
@@ -1,11 +1,17 @@
 <?php
 
+use classes\Imc;
 use classes\Paciente;
 
     include 'autoload.php';
 
     $paciente1 = new Paciente('Pedro', 22, 1.7, 79);
-    $paciente1->calcImc();
-    $paciente1->showImc();
+    /* $paciente1->calcImc();
+    $paciente1->showImc(); */
+
+    $paciente2 = new Paciente('Luis', 21, 1.8, 55);
+    $imc = new Imc();
+    $imc::calc($paciente1);
+    $imc::calc($paciente2);
 
 ?>
