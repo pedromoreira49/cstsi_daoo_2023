554a1efd2a90b4d4bd8fa5f2689f7cdbb555cc0a
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-25 01:12:23 -0300

    feat: adicionado funções para controller usuarios

--- sem05_laravel/app/Http/Controllers/UsersController.php
+++ sem05_laravel/app/Http/Controllers/UsersController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Users;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //
+    private $users;
+
+    public function __construct(){
+        $this->users = new Users();
+    }
+
+    public function index(){
+        return view('users', ['users'=>$this->users->all()]);
+    }
+
+    public function show($id){
+        return view('single-user', ['user'=>$this->users->find($id)]);
+    }
 }
