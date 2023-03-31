2778d6393beedd3e5ef78b36997ece6a23a7c241
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-31 20:42:30 -0300

    feat: adicionado view para atualizar usuários

---
+++ sem05_laravel/resources/views/users/user_edit.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Update User</h1>
+    <form action="{{route('updateUser',$user->id)}}" method="POST">
+        @csrf
+        <table>
+            <tr>
+                <td>Nome:</td>
+                <td><input type="text" name="nome" value="{{$user->nome}}"/></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="text" name="email"value="{{$user->email}}"/></td>
+            </tr>
+            <tr>
+                <td>Senha:</td>
+                <td><input type="password" name="senha" value="{{$user->senha}}"/></td>
+            </tr>
+            <tr align="center">
+                <td colspan="2"><input type="submit" value="Atualizar"/></td>
+            </tr>
+            <tr align="center">
+                <td colspan="2"><a href="/users" style="display: inline">&#9664;&nbsp;Voltar</a></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
