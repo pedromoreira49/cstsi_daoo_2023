1edbcd87818cba2209cb1951c9374aba91b9c431
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-04-01 19:50:54 -0300

    feat: adicionado função para criar agendamento

--- sem05_laravel/app/Http/Controllers/AgendamentosController.php
+++ sem05_laravel/app/Http/Controllers/AgendamentosController.php
@@ -14,10 +14,26 @@ class AgendamentosController extends Controller
     }
 
     public function index(){
-        return view('agendamentos', ['agendamentos'=>$this->agendamentos->all()]);
+        return view('agendamentos/agendamentos', ['agendamentos'=>$this->agendamentos->all()]);
     }
 
     public function show($id){
-        return view('single-agendamento', ['agendamento'=>$this->agendamentos->find($id)]);
+        return view('agendamentos/single-agendamento', ['agendamento'=>$this->agendamentos->find($id)]);
+    }
+
+    public function createAgendamento(){
+        return view('agendamentos/agendamento_create');
+    }   
+
+    public function storeAgendamento(Request $request){
+        $agendamento = new Agendamentos;
+        $array_input_request = $request->all();
+        $agendamento->fill($array_input_request);
+
+        if($agendamento->save()){
+            return redirect('/agendamentos');
+        }else{
+            dd("Erro ao cadastrar agendamento!");
+        }
     }
 }
