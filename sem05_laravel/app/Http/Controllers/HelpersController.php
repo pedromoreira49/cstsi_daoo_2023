131721f6885cdec133460fc2759f7e485cb0a4fc
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-03-31 21:19:40 -0300

    feat: adicionado função para criar ajudantes

--- sem05_laravel/app/Http/Controllers/HelpersController.php
+++ sem05_laravel/app/Http/Controllers/HelpersController.php
@@ -14,11 +14,27 @@ class HelpersController extends Controller
     }
 
     public function index(){
-        return view('helper', ['helpers'=>$this->helper->all()]);
+        return view('helpers/helper', ['helpers'=>$this->helper->all()]);
     }
 
     public function show($id){
-        return view('single-helper', ['helper'=>$this->helper->find($id)]);
+        return view('helpers/single-helper', ['helper'=>$this->helper->find($id)]);
+    }
+
+    public function createHelper(){
+        return view('helpers/helper_create');
+    }   
+
+    public function storeHelper(Request $request){
+        $helper = new Helpers;
+        $array_input_request = $request->all();
+        $helper->fill($array_input_request);
+
+        if($helper->save()){
+            return redirect('/helpers');
+        }else{
+            dd("Erro ao cadastrar ajudante!");
+        }
     }
 
 }
