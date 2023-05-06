703e8f5fe371667692c2cfdf8fb0047ffcdb29ab
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-05-06 18:41:14 -0300

    feat: adicionado View para Home do sistema

---
+++ sem05_laravel/resources/views/Home/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Produtos</title>
+</head>
+<body>
+    <h1>Funções</h1>
+    <p><a href="{{route('produtos')}}" title="Ver">Visualizar produtos</a></p>
+    <p><a href="{{route('users')}}" title="Ver">Visualizar usuarios</a></p>
+    <p><a href="{{route('agendamentos')}}" title="Ver">Visualizar agendamentos</a></p>
+    <p><a href="{{route('helpers')}}" title="Ver">Visualizar ajudantes</a></p>
+</body>
+</html>
