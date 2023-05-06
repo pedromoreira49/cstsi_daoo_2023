5fd4044ae165ffc77a12daf4a2341c4fec478b6e
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-05-06 18:25:45 -0300

    refac: alterada função de boot em App Service Provider para testar Auth do Supabase

--- sem05_laravel/app/Providers/AppServiceProvider.php
+++ sem05_laravel/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \PHPSupabase\Service;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /* $service = new Service(
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlb3pxZXN0dGl6d3hlemdkY2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMzMDg3NjksImV4cCI6MTk5ODg4NDc2OX0.8C9v5J_9g-cI9v0yonHdPkg52b5j7s_qS4PEyfZ3HxQ',
+            'https://keozqesttizwxezgdcgz.supabase.co'
+        );
+        $auth = $service->createAuth();
+        Log::channel('single')->info(print_r($auth,true)); */
     }
 }
