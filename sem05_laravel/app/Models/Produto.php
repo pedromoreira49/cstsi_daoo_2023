ee9a23264608d2fba39c4192fcf4f2899efe41e9
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-05-12 15:48:31 -0300

    refac: alterado estrutura da Model de Produto

--- sem05_laravel/app/Models/Produto.php
+++ sem05_laravel/app/Models/Produto.php
@@ -8,5 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome', 'descricao', 'qtd_estoque', 'preco', 'importado'];
+
+    protected $fillable = [
+        "nome",
+        "descricao",
+        "preco",
+        "qtd_estoque",
+        "importado",
+		"fornecedor_id"
+    ];
+
+    public function fornecedor(){
+        return $this->belongsTo(Fornecedor::class);
+    }
 }
