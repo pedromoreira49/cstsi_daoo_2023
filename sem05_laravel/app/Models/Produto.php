702bbe6c1fc0fe5f4a795e9fcdaecb7c3a7e7177
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-05-28 18:33:16 -0300

    refac: alterado estrutura da função de Promocoes

--- sem05_laravel/app/Models/Produto.php
+++ sem05_laravel/app/Models/Produto.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    use HasFactory;
+    use HasFactory, \Znck\Eloquent\Traits\BelongsToThrough;
 
     protected $fillable = [
         "nome",
@@ -21,4 +21,26 @@ class Produto extends Model
     public function fornecedor(){
         return $this->belongsTo(Fornecedor::class);
     }
+
+    public function regiao(){
+        return $this->belongsToThrough(
+            Regiao::class,
+            [
+                Estado::class,
+                Fornecedor::class
+            ],
+            null,
+            '',
+            [
+                Regiao::class=>'regiao_id',
+                Fornecedor::class=>'fornecedor_id'
+            ]
+        );
+    }
+
+    public function promocoes(){
+        return $this->belongsToMany(Promocao::class)
+          ->withPivot('desconto')
+          ->withTimestamps();
+      }
 }
