f4d6134e1bec034013d259925c7c8f3f3382afeb
pedromoreira49 <jpedrostonemoreira@gmail.com>
2023-05-28 18:33:33 -0300

    refac: alterado estrutura Regiao

--- sem05_laravel/app/Models/Regiao.php
+++ sem05_laravel/app/Models/Regiao.php
@@ -7,12 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Regiao extends Model
 {
-    use HasFactory;
+    use HasFactory, \Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
     protected $table = 'regioes';
     protected $fillable = ['nome'];
 
     public function estados(){
         return $this->hasMany(Estado::class);
+    }
+
+    public function fornecedores(){
+        return $this->hasManyThrough(Fornecedor::class, Estado::class);
+    }
+
+    public function produtos()
+    {
+        return $this->hasManyDeep(
+            Produto::class,
+            [Estado::class,Fornecedor::class]
+        );
     }
 }
